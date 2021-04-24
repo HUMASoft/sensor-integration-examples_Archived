0d3e2bee58ce4381b1c701697d3c7519b6d65791
buyus <jmyanezb@ing.uc3m.es>
2021-04-24 08:46:13 +0200

    modificado:     CMakeLists.txt 	modificado:     lib/sensor-integration 	modificado:     src/example_imu3dmgx510.cpp

--- src/example_imu3dmgx510.cpp
+++ src/example_imu3dmgx510.cpp
@@ -7,28 +7,43 @@ int main()
 {
 
 
-    SerialComm port;
-    string ret;
+//    SerialComm port;
+//    string ret;
+//    string expect("\x75\x65\x01\x04\x04\xF1\x02\x00\xD6\x6C");
 
 //    for(int i=0;i<2;i++)
 //    {
 //        port.WriteLine("\x75\x65\x01\x02\x02\x02\xe1\xc7");
-//        cout << port.ReadUntil("l") <<endl;
+//        ret= port.ReadUntil('l');
+//        cout << ret <<endl;
 //        cout << "\x75\x65\x01\x04\x04\xF1\x02\x00\xD6\x6C"<<endl;
 //    }
+
+//    for(uint i=0;i<ret.size();i++)
+//    {
+//        cout << int(ret[i]) << ",";
+//    }
+//    cout <<endl;
+//    for(uint i=0;i<expect.size();i++)
+//    {
+//        cout << int(expect[i]) << ",";
+//    }
+//    cout <<endl;
+//    ret.resize(expect.size());
+//    cout << ret.compare(expect) << endl;
 
 
     IMU3DMGX510 imu("/dev/ttyUSB0",100);
 
-    imu.set_IDLEmode();
-    imu.set_devicetogetgyroacc();
-    imu.set_streamon();
-    vector<double> a1(2);
-    double* a2 = imu.EulerAngles();
-    for (int i=0; i<10; i++)
-    {
-        cout << a2[0] << ", " << a2[1] << endl;
-    }
+//    imu.set_IDLEmode();
+//    imu.set_devicetogetgyroacc();
+//    imu.set_streamon();
+//    vector<double> a1(2);
+//    double* a2 = imu.EulerAngles();
+//    for (int i=0; i<10; i++)
+//    {
+//        cout << a2[0] << ", " << a2[1] << endl;
+//    }
 
     return 0;
 }
