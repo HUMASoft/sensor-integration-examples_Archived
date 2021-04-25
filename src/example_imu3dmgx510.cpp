08334ca4e5bcf9586f018e4aea1eade184b471c4
munozyanez <munoz.yanez@gmail.com>
2021-04-25 17:18:49 +0200

    modificado:     lib/sensor-integration 	modificado:     src/example_imu3dmgx510.cpp

--- src/example_imu3dmgx510.cpp
+++ src/example_imu3dmgx510.cpp
@@ -7,39 +7,14 @@ int main()
 {
 
 
-//    SerialComm port;
-//    string ret;
-//    string expect("\x75\x65\x01\x04\x04\xF1\x02\x00\xD6\x6C");
-
-//    for(int i=0;i<2;i++)
-//    {
-//        port.WriteLine("\x75\x65\x01\x02\x02\x02\xe1\xc7");
-//        ret= port.ReadUntil('l');
-//        cout << ret <<endl;
-//        cout << "\x75\x65\x01\x04\x04\xF1\x02\x00\xD6\x6C"<<endl;
-//    }
-
-//    for(uint i=0;i<ret.size();i++)
-//    {
-//        cout << int(ret[i]) << ",";
-//    }
-//    cout <<endl;
-//    for(uint i=0;i<expect.size();i++)
-//    {
-//        cout << int(expect[i]) << ",";
-//    }
-//    cout <<endl;
-//    ret.resize(expect.size());
-//    cout << ret.compare(expect) << endl;
-
     uint freq=100;
-    IMU3DMGX510 imu("/dev/ttyUSB0",freq);
+    IMU3DMGX510 imu("/dev/ttyUSB1",freq);
 
 //    imu.set_IDLEmode();
 //    imu.set_devicetogetgyroacc();
 //    imu.set_streamon();
 //    vector<double> a1(2);
-    double* a2;
+    double* angles;
     double pitch,roll;
 
     double dts=1.0/freq;
@@ -47,7 +22,8 @@ int main()
     IPlot plRoll(dts);
 
 
-    for (int i=0; i<600*freq; i++)
+
+    for (int i=0; i<6*freq; i++)
     {
         imu.GetPitchRoll(pitch,roll);
 //        cout << pitch << ", " << roll << endl;
@@ -55,12 +31,20 @@ int main()
         plPitch.pushBack(pitch);
         plRoll.pushBack(roll);
 
-//        a = imu.EulerAngles();
-//        a2 = imu.get_euleranglesPolling();
-//        cout << a2[0] << ", " << a2[1] << endl;
-//        cout << i << endl;
-//        usleep(1000*1000*dts);
     }
+
+//    imu.set_streamon();
+//    for (int i=0; i<6*freq; i++)
+//    {
+//            angles = imu.EulerAngles();
+//            cout << angles[0] << ", " << angles[1] << endl;
+//            cout << i << endl;
+//            usleep(1000*1000*dts);
+//        plPitch.pushBack(angles[0]);
+//        plRoll.pushBack(angles[1]);
+
+
+//    }
 
     plPitch.Plot();
     plRoll.Plot();
